d8813768023ee82510be530f546e8d0315cc1267
kevblond <kevinblondel@msn.com>
2016-04-26 19:12:17 +0200

    hpp

---
+++ include/Triangle.hpp
@@ -0,0 +1,23 @@
+
+#ifndef PROJECT_Triangle_HPP
+#define PROJECT_Triangle_HPP
+
+class Triangle {
+protected :
+	Point p1;
+	Point p2;
+	Point p3;
+public :
+	Triangle(void);
+	Triangle(Point p1,Point p2,Point p3);
+	
+	~Triangle();
+	bool operator==(const Triangle &p);
+    bool operator!=(const Triangle &p);
+    Triangle operator=(const Triangle &p);
+    float getP1() const;
+    float getP2() const;
+    float getP3() const;
+};
+
+#endif
